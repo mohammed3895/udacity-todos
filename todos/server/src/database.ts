0fb18cca3ea127243d6bb47a333dda7c9a08dc9c
mohammed samir <85784627+mohammed3895@users.noreply.github.com>
2023-01-06 18:33:04 +0200

    Update database.ts

--- todos/server/src/database.ts
+++ todos/server/src/database.ts
@@ -1,21 +1,12 @@
-import dotenv from 'dotenv'
 import { Pool } from 'pg'
-
-dotenv.config()
-const {
-    POSTGRES_HOST,
-    POSTGRES_DB,
-    POSTGRES_USER,
-    POSTGRES_PASSWORD,
-} = process.env
-
+import { config } from './config/config'
 
 
 const client = new Pool({
-    host: POSTGRES_HOST,
-    database: POSTGRES_DB,
-    user: POSTGRES_USER,
-    password: POSTGRES_PASSWORD,
+    host: config.host,
+    database: config.database,
+    user: config.username,
+    password: config.password,
 })
 
 export default client
