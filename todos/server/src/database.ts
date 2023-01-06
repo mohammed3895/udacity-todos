74d776f9c4d50e361a574e944b8b462ef84c81c0
mohammed samir <85784627+mohammed3895@users.noreply.github.com>
2023-01-06 17:56:20 +0200

    Update database.ts

--- todos/server/src/database.ts
+++ todos/server/src/database.ts
@@ -5,17 +5,15 @@ dotenv.config()
 const {
     POSTGRES_HOST,
     POSTGRES_DB,
-    POSTGRES_TEST_DB,
     POSTGRES_USER,
     POSTGRES_PASSWORD,
-    ENV
 } = process.env
 
 
 
 const client = new Pool({
     host: POSTGRES_HOST,
-    database: ENV === 'dev' ? POSTGRES_DB : POSTGRES_TEST_DB,
+    database: POSTGRES_DB,
     user: POSTGRES_USER,
     password: POSTGRES_PASSWORD,
 })
