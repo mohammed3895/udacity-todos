5a6dcd128b3df61198d44cc6c1c5836941b2a6c0
mohammed samir <85784627+mohammed3895@users.noreply.github.com>
2023-01-07 17:14:50 +0200

    Update set_env.sh

--- todos/server/set_env.sh
+++ todos/server/set_env.sh
@@ -1,1 +1,5 @@
-eb setenv POSTGRES_HOST=database-1.cu0xr9ccvyp4.us-east-1.rds.amazonaws.com POSTGRES_USER=postgres POSTGRES_PASSWORD=pass123456 POSTGRES_DB=postgres PORT=8080
+eb setenv POSTGRES_HOST=database-1.cu0xr9ccvyp4.us-east-1.rds.amazonaws.com 
+eb setenv POSTGRES_USER=postgres 
+eb setenv POSTGRES_PASSWORD=pass123456 
+eb setenv POSTGRES_DB=postgres 
+eb setenv PORT=8080
