08dde98238af580f0802437f636ca6f5a639456b
mohammed samir <85784627+mohammed3895@users.noreply.github.com>
2023-01-07 14:38:37 +0200

    eb setenv

--- todos/server/set_env.sh
+++ todos/server/set_env.sh
@@ -1,6 +1,7 @@
-export POSTGRES_USER=postgres
-export POSTGRES_PASSWORD=pass123456
-export POSTGRES_HOST=database-1.cu0xr9ccvyp4.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::todos2022
-export AWS_REGION=us-east-1
+eb setenv POSTGRES_HOST=database-1.cu0xr9ccvyp4.us-east-1.rds.amazonaws.com
+eb setenv POSTGRES_USER=postgres
+eb setenv POSTGRES_PASSWORD=pass123456
+eb setenv POSTGRES_DB=postgres
+eb setenv PORT=8080
+eb setenv AWS_BUCKET=arn:aws:s3:::todos2022
+eb setenv AWS_REGION=us-east-1
